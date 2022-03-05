b72259a84ee77e67ecd27446dd8dca21422b2165
vichanley <viccantu23@gmail.com>
2022-03-05 10:11:06 -0600

    player object

--- __tests__/player.test.js
+++ __tests__/player.test.js
@@ -1,4 +1,7 @@
 const Player = require('../lib/player');
+const Potion = require('../lib/Potion');
+
+jest.mock('../lib/Potion');
 
 test('creates a player object', () => {
     const player = new Player('Dave');
@@ -7,4 +10,7 @@ test('creates a player object', () => {
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+      );
   });
